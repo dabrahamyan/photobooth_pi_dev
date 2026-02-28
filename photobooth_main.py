879c070233bdfe2db2173aecaa7b85e1a7de3744
David <abrahamyan.d@northeastern.edu>
2026-02-28 13:38:16 -0500

    moved photo down a bit for aspect ratio change

--- photobooth_main.py
+++ photobooth_main.py
@@ -119,7 +119,7 @@ def take_photo_and_print():
 
         # Paste stuff
         if qr:
-            template.paste(pic, (10, 145))
+            template.paste(pic, (10, 180))
             template.paste(qr, (185, 730))
         else:
             template.paste(pic, (10, 145))
